393d12d4e2a62e0c41334081de1d0bd92f8fb9aa
Noah Grossman <noahbgrossman@gmail.com>
2018-04-06 18:32:07 -0400

    More from initial commit

---
+++ js/viewport.js
@@ -0,0 +1,10 @@
+$(window).scroll(function() {
+  	$('video').each(function(){
+	    if ($(this).is(":in-viewport")) {
+	        $(this)[0].play();
+	    } else {
+	        $(this)[0].pause();
+	    }
+	})
+});
+
